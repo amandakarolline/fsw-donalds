a5f5161599b5c43fa9e0230730c43ad87b98628e
Amanda <amandakarolline@hotmail.com>
2025-02-20 11:48:49 -0300

    chore: update css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,7 +6,7 @@ const ProductPage = () => {
     <div className="p-5 border border-red-500 rounded-xl">
         <h1 className="text-red-500">Products Page</h1>
         <Button>FSW 7.0</Button>
-        <input placeholder="Bora fechar esse projeto" />
+        <Input placeholder="Bora fechar esse projeto" />
     </div>
     );
 }
